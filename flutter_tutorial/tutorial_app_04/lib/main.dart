eaa1a1350d0dc2c16ef8be8282fa16d1cc43918a
Luciano Silva <luciano.tj2011@gmail.com>
2018-09-14 14:12:15 -0300

    Flutter Tutorial Video 04 - Using Navigation and Routes

---
+++ flutter_tutorial/tutorial_app_04/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+/*
+* Navigation and Routes
+* */
+
+void main() => runApp(MaterialApp(
+    home: HomePage(),
+    routes: <String, WidgetBuilder> { // define all our routes
+      // the home page is automatically defined but can do this:
+      // "/": (BuildContext context) => HomePage(),
+      "/SecondPage": (BuildContext context) => SecondPage()
+    },
+  )
+);
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home'), backgroundColor: Colors.deepOrange),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.favorite, color: Colors.redAccent),
+                iconSize: 70.0,
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/SecondPage");
+                },
+              ),
+              Text("Home")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Page'), backgroundColor: Colors.deepOrange),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.home, color: Colors.blue),
+                iconSize: 70.0,
+                onPressed: null,
+              ),
+              Text("Second Page")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
